adae7b3a70161b5b047765e6a760739641a37017
ThankY0u <1208522@student.roc-nijmegen.nl>
2026-06-03 13:22:38 +0200

    auth opzet af
    momenteel kan je een account aanmaken en inloggen, ik zal verder werken aan uitbereiding hieraan

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,25 +1,23 @@
-
-
 import { useState } from 'react';
-import { useNavigate, Navigate } from 'react-router';
 import { supabase } from '../supabase';
 import { useSession } from '../hooks/useSession';
-import './Auth.css';
 
+export default function Home() {
+  const [content, setContent] = useState("");
+  const { session } = useSession();
 
-export default function Home(){
-const [content, setContent] = useState("");
-const {session} = useSession();
-function submitHandler(event){
+  async function submitHandler(event) {
     event.preventDefault();
-
-    const {error } = await supabase.from("posts").insert({
-        user_id: session.sub,
-        content: content
+    const { error } = await supabase.from("posts").insert({
+      user_id: session.sub,
+      content: content
     });
     if (!error) setContent("");
-}
-return
-  
+  }
 
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 }
